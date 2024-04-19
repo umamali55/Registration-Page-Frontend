eaecee359772f569f9013d4d74ed7316642be89a
umamali55 <159407497+umamali55@users.noreply.github.com>
2024-04-19 23:06:44 +0530

    Add

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Registration from './Components/Registration';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Registration />
+    </div>
+  );
+}
+
+export default App;
+
